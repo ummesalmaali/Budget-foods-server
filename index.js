141f268cfa19ca9faf136707d4272e37aab72982
Umme Salma Ali <ummes3527@gmail.com>
2021-04-02 01:42:56 +0600

    done

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express')
+
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const port = 5000
+
+
+const app = express()
+
+app.use(bodyParser.json());
+app.use(cors());
+
+
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://DB_USER:DB_PASS@cluster0.npsqi.mongodb.net/DB_NAME?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const foodCollection = client.db('budget-foods').collection("foods");
+  console.log('database connected successfully')
+  app.post('/addFood',(req,res) => {
+    const foods = req.body;
+    foodCollection.insertMany(foods)
+    .then(result => {
+        console.log(result.insertedCount);
+        res.send(result.insertedCount);
+    })
+  })
+  app.get('/foods',(req,res) => {
+      foodCollection.find({})
+      .toArray((err,documents) =>{
+          res.send(documents);
+      })
+  })
+});
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port);
