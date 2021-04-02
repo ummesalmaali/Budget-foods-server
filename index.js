06d196a2d6eedae9d61235bfc17000f07b2ec599
Umme Salma Ali <ummes3527@gmail.com>
2021-04-02 16:14:47 +0600

    done

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(cors());
 
 
 const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://DB_USER:DB_PASS@cluster0.npsqi.mongodb.net/DB_NAME?retryWrites=true&w=majority";
+const uri = "mongodb+srv://budget:budget77@cluster0.npsqi.mongodb.net/budget-foods?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
   const foodCollection = client.db('budget-foods').collection("foods");
