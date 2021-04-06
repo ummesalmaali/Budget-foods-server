cac496f28f21c4a165976e1e2e106e5860e319cc
Umme Salma Ali <ummes3527@gmail.com>
2021-04-06 21:22:10 +0600

    added

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require('express')
 
 const bodyParser = require('body-parser');
 const cors = require('cors');
+require('dotenv').config()
+const ObjectID = require('mongodb').ObjectID
 
-const port = 5000
-
+const port = process.env.PORT || 5000;
+console.log(process.env.DB_USER);
 
 const app = express()
 
@@ -14,17 +16,24 @@ app.use(cors());
 
 
 const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.npsqi.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority";
+// const { ObjectID } = require('bson');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.npsqi.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
+  console.log('connection err',err);
   const foodCollection = client.db('budget-foods').collection("foods");
-  console.log('database connected successfully')
-  app.post('/addFood',(req,res) => {
-    const foods = req.body;
-    foodCollection.insertMany(foods)
+  console.log('database connected');
+
+    app.post('/addFood',(req,res) => {
+    const newFood = req.body;
+    console.log('adding new data', newFood);
+
+    foodCollection.insertOne(newFood)
     .then(result => {
+      console.log(result);
         console.log(result.insertedCount);
-        res.send(result.insertedCount);
+        res.send(result.insertedCount > 0);
     })
   })
   app.get('/foods',(req,res) => {
@@ -33,11 +42,27 @@ client.connect(err => {
           res.send(documents);
       })
   })
+  app.get('/foods/:id',(req,res) => {
+    console.log(req.params.id)
+      foodCollection.find({_id : ObjectID(req.params.id)})
+      .toArray((err,documents) =>{
+          res.send(documents);
+      })
+  })
 });
+app.delete('/delete/:id',(req,res) => {
+  const id = ObjectID(req.params.id)
+  console.log('delete this', id);
+  foodCollection.deleteOne({_id:id})
+  .then((result) =>{
+    console.log(result);
+    res.send(result)
+  })
+})
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('hey...whats up?!')
 })
 
 app.listen(process.env.PORT || port);
